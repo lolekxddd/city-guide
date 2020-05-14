6dac81233ea7a004a54d30219000069719b4752d
saros <60976422+lolekxddd@users.noreply.github.com>
2020-05-14 12:54:46 +0200

    Update urls.py

--- src/city_guide/urls.py
+++ src/city_guide/urls.py
@@ -5,4 +5,6 @@ from rest_auth.registration.views import VerifyEmailView, RegisterView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('accounts.api.urls')),
+    path('api/attraction/', include(('attraction.api.urls', 'attraction.api')),
+    path('api/plan/', include(('plan.api.urls', 'plan.api')),
 ]
